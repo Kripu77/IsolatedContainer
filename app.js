37d8f0977071f31ac3350142ba1e024f950de457
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2022-04-29 22:11:54 +1000

    sever addition

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const http = require('http');
 const port =8080;
 
 http.createServer((req, res)=>{
-res.write('hola')
-res.end();
+    if(req.url === '/') res.end('You are in the homepage')
+    // res.end(req.url)
+    if(req.url ==='/about') res.end('you are in the about page')
+//     console.log(req.url)
+//    res.write("node server running.")
+// res.end();
 }).listen(port)
