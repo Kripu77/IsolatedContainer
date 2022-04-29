bb89219a4f2fa8ef2767d8cc716bcd8968abf56b
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2022-04-29 19:20:26 +1000

    updated content

--- index.js
+++ index.js
@@ -3,3 +3,24 @@
 // const os = require('os')
 
 // console.log(`The uptime of the system is ${os.uptime()*60} minutes`)
+
+// console.log(os.userInfo().username)
+
+
+// console.log(`The user of this system is ${os.userInfo().username}`)\\
+
+
+// const path = require('path');
+
+// // console.log(path)
+
+// // // console.log(path.join('/test', 'sub', 'app'))
+
+// // const absolute = path.resolve(__dirname, 'app.js')
+
+// // console.log(absolute)
+
+//file system module
+
+const {readFileSync} = require('fs');
+console.log(readFileSync())
