ed2c1378b63d963125cc3cf2b5f929670d99f24e
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2022-07-07 21:16:47 +1000

    query route

--- node-express/query.js
+++ node-express/query.js
@@ -14,12 +14,34 @@ res.status(200).send("<h1> Welcome to the Home Page </h1>");
 
 
 //query params
+function template(data){
+    const htmlTemp = `<div>
+    <h1> Student Names: </h1>
+    <br/>
+    <h2> ${data.name}</h2>
+     </div>`
+     return htmlTemp
+}
 
 app.get("/student/search", (req, res)=>{
 
     console.log(req.query);
+    const queryParams = req.query;
+    let sortedStudents = [...data]
+    const string = 'Hello'
+    console.log(string.toLocaleLowerCase().startsWith('h'))
 
-    res.send("<h1> view list of students available </h1>");
+    sortedStudents = sortedStudents.filter((student)=>{
+        console.log(student.name)
+        return student.name.toLowerCase().startsWith(queryParams.startsWith)
+    })
+    console.log(sortedStudents)
+    const studentList = sortedStudents.slice(0,queryParams.limit).map((student)=>{ 
+       
+        return template(student)
+    })
+
+    res.send(`<h1> view list of students available ${studentList}</h1>`);
 
 })
 //catch all route
